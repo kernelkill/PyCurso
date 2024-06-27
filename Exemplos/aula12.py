57fa21b049e107aa631f26a4744eca415dc24b18
kachorroski <kernelkill@outlook.com>
2024-06-27 15:28:37 -0400

    Exercicio 12 em andamento

---
+++ Exemplos/aula12.py
@@ -0,0 +1,11 @@
+nome = 'Joabe Kachorroski'
+altura = 1.75
+peso = 95
+imc = peso / altura ** 2
+
+linha1 = f'{nome} tem {altura:.2f} de altura,'
+linha2 = f'pesa {peso} quilos e seu imc e {imc:.2f}'
+
+print(linha1)
+print(linha2)
+#print()
