6d52e642f7f5d249353c5bfa1a6a05566afd3dff
kachorroski <kernelkill@outlook.com>
2024-06-27 16:26:04 -0400

    aqui tem um comentario

---
+++ Exemplos/aula14.py
@@ -0,0 +1,14 @@
+
+a = ''
+b = ''
+c = ''
+
+nome = "Luiz"
+idade = 23
+formato = '{n} tem {i} anos'
+#print(formato.format(n=nome, i=idade))
+formato = f'{nome} tem {idade:.2f} anos'
+print(formato)
+
+soma = 10 * 20
+print(soma)
---
+++ Exemplos/aula16.py
@@ -0,0 +1,9 @@
+
+entrada = input('Voce quer entrar ou sair? ')
+
+if entrada == 'entrar':
+    print('Voce entrou no sistema')
+elif entrada == 'sair':
+    print('voce saiu do sistema')
+else:
+    print('voce nao digiou nada arrombado')
